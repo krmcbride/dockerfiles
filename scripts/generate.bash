88dcad5cbe27d24a85f6842e06982a36c802e6b5
Kevin McBride <krmcbride.io@gmail.com>
2017-07-08 23:38:49 -0700

    Add debian to generation script

--- scripts/generate.bash
+++ scripts/generate.bash
@@ -4,9 +4,21 @@ set -eo pipefail
 [[ "$TRACE" ]] && set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )"
 
-echo "generating base dockerfiles"
-alpine_versions=(3.4 3.5 3.6)
-for v in ${alpine_versions[@]}; do
-    echo "generating alpine:${v}-dev Dockerfile"
-    sed 's~^FROM.*$~FROM krmcbride/alpine:'"${v}"'-base~' "${DIR}/base/alpine/Dockerfile.dev" > "${DIR}/base/alpine/${v}-dev/Dockerfile"
-done
+generate_base () {
+    alpine_versions=(3.4 3.5 3.6)
+    debian_versions=(jessie)
+
+    echo "generating base dockerfiles"
+
+    for v in ${alpine_versions[@]}; do
+        echo "generating alpine:${v}-dev Dockerfile"
+        sed 's~^FROM.*$~FROM krmcbride/alpine:'"${v}"'-base~' "${DIR}/base/alpine/Dockerfile.dev" > "${DIR}/base/alpine/${v}-dev/Dockerfile"
+    done
+
+    for v in ${debian_versions[@]}; do
+        echo "generating debian:${v}-dev Dockerfile"
+        sed 's~^FROM.*$~FROM krmcbride/debian:'"${v}"'-base~' "${DIR}/base/debian/Dockerfile.dev" > "${DIR}/base/debian/${v}-dev/Dockerfile"
+    done
+}
+
+generate_base
