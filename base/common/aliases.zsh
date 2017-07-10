8ef8dc54d5a7a068f5171a02a5cb9231f8b62fd6
Kevin McBride <krmcbride.io@gmail.com>
2017-07-09 21:44:41 -0700

    Remove extraneous zsh aliases

--- base/common/aliases.zsh
+++ base/common/aliases.zsh
@@ -1,20 +1,9 @@
 
-# hub
-alias git=hub
-
 # shell
 alias c='clear'
 alias l='ls -hl'
 alias all='l -A'
 alias ll='l -A'
-
-# Docker
-alias docker_stats='docker stats $(docker ps|grep -v "NAMES"|awk '\''{ print $NF }'\''|tr "\n" " ")'
-alias docker_syslog='syslog -w boot -k Sender Docker'
-alias random_port="docker run -it --rm krmcbride/ubuntu:14.04 shuf -i 32768-61000 -n 1"
-alias lo0_alias='sudo ifconfig lo0 alias 172.16.123.1'
-#alias docker_xhyve='screen ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty'
-alias docker_xhyve='docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i sh'
 
 # Git
 alias g='git'
@@ -34,7 +23,3 @@ alias ggs="gg --stat"
 alias gnew="git log HEAD@{1}..HEAD@{0}"
 # Add uncommitted and unstaged changes to the last commit
 alias gcaa="git commit -a --amend -C HEAD"
-
-
-alias java-repl='java -jar ~/Sites/GitHub/albertlatacz/java-repl/build/libs/javarepl-dev.jar'
-
